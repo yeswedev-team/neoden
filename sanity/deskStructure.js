f657dc0379e2506bcc088a4f6f93884ca7bcc6c7
David Gourdet <davidgourdet@mac-1.home>
2020-12-11 21:55:36 +0100

    Add deskStructure.js for Structure Builder (cf. Sanity doc)

---
+++ sanity/deskStructure.js
@@ -0,0 +1,23 @@
+import S from '@sanity/desk-tool/structure-builder';
+
+const hiddenDocTypes = (listItem) =>
+  !['menuSingleton'].includes(listItem.getId());
+
+export default () =>
+  S.list()
+    .title('Content')
+    .items([
+      S.listItem()
+        .title('Site')
+        .child(
+          S.editor().schemaType('singletonSite').documentId('singletonSite')
+        )
+        .title('Menu principal')
+        .child(
+          S.editor().schemaType('singletonMenu').documentId('singletonMenu')
+        ),
+      // Add a visual divider (optional)
+      S.divider(),
+      // List out the rest of the document types, but filter out the config type
+      ...S.documentTypeListItems().filter(hiddenDocTypes),
+    ]);
