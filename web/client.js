b7d5aad2a407124d896e8713f582c1f2b51b0449
David Gourdet <davidgourdet@mac.home>
2020-12-23 20:32:52 +0100

    Remove NextJS

--- web/client.js
+++
@@ -1,9 +0,0 @@
-import sanityClient from '@sanity/client'
-const client = sanityClient({
-  projectId: process.env.SANITY_PROJECT_ID,
-  dataset: 'production',
-  token: process.env.SANITY_TOKEN, // or leave blank to be anonymous user
-  useCdn: false // `false` if you want to ensure fresh data
-})
-
-export default client
