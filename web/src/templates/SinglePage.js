702d2af3840e54d2ae18a761f12a4a0a5e50d28a
David Gourdet <davidgourdet@mac.home>
2020-12-26 12:07:56 +0100

    Implement Map to components for page sections

--- web/src/templates/SinglePage.js
+++ web/src/templates/SinglePage.js
@@ -1,10 +1,33 @@
 import React from 'react';
 import { graphql } from 'gatsby';
+import { MapToComponents } from 'react-map-to-components';
+import PortableText from '../components/PortableText';
+import Blocks from '../components/sections/Block';
+import TwoColumnsWithOverlayedImages from '../components/sections/TwoColumnsWithOverlayedImages';
 
-export default function HomePage({ pageContext, data: { singlePage } }) {
+export default function SinglePage({ pageContext, data: { singlePage } }) {
   console.log(pageContext);
-  const { title } = singlePage.page;
-  return <h1>{title.fr}</h1>;
+  const { title, content, _rawText } = singlePage.page;
+  console.log(content);
+  return (
+    <>
+      <section className="intro-text">
+        <div className="container container--lg">
+          <h1>{title.fr}</h1>
+          <PortableText blocks={_rawText} />
+        </div>
+      </section>
+      <MapToComponents
+        getKey={(section) => section.id || section._key}
+        getType={(section) => section._type}
+        list={content}
+        map={{
+          blocks: Blocks,
+          ctaColumns: TwoColumnsWithOverlayedImages,
+        }}
+      />
+    </>
+  );
 }
 
 export const query = graphql`
@@ -15,6 +38,19 @@ export const query = graphql`
           fr
         }
         _rawText(resolveReferences: { maxDepth: 10 })
+        content {
+          ... on SanityBlocks {
+            id
+            _key
+            _type
+            title
+            _rawBlock(resolveReferences: { maxDepth: 10 })
+          }
+          ... on SanityCtaColumns {
+            _key
+            _type
+          }
+        }
       }
     }
   }
