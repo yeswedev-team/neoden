ba589857b3012ed522f480f094fde090376ec73c
David Gourdet <davidgourdet@mac.home>
2021-01-24 21:59:20 +0100

    Disable waves animations on small screens

--- web/src/components/Wave.js
+++ web/src/components/Wave.js
@@ -1,20 +1,34 @@
 /* eslint-disable no-plusplus */
-import React, { useRef, useEffect } from 'react';
+import React, { useEffect } from 'react';
 import Wave from 'react-wavify';
 
 const Wavify = (props) => {
-  const canvasRef = useRef(null);
   const { bgcolor, direction } = props;
 
-  console.log(canvasRef.current);
+  let windowWidth = 0;
 
-  useEffect(() => {}, [canvasRef, bgcolor]);
+  if (typeof window !== 'undefined') {
+    windowWidth = window.innerWidth;
+  }
+
+  const [width, setWidth] = React.useState(windowWidth);
+  const breakpoint = 1280;
+
+  useEffect(() => {
+    const handleResizeWindow = () => setWidth(window.innerWidth);
+    // subscribe to window resize event "onComponentDidMount"
+    window.addEventListener('resize', handleResizeWindow);
+    return () => {
+      // unsubscribe "onComponentDestroy"
+      window.removeEventListener('resize', handleResizeWindow);
+    };
+  }, []);
 
   return (
     <Wave
       className={`${direction === 'down' ? 'wave-down' : 'wave-up'}`}
       fill={bgcolor}
-      paused={false}
+      paused={width < breakpoint}
       options={{
         height: 20,
         amplitude: 20,
