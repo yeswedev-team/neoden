8bec8377bf83092a15d371c5e750c797eabca4bf
Violette Hurel <violette.hurel@gmail.com>
2021-01-27 16:47:04 +0100

    Create 404 WIP

---
+++ web/src/pages/404.js
@@ -0,0 +1,14 @@
+import React from 'react';
+// import BlogPostPreviewList from '../components/blog/BlogPostPreviewList';
+
+export default function FourOhFourPage() {
+  return (
+    <>
+      {/* <BlogPostPreviewList /> */}
+      <h1 className="page__title">404</h1>
+      <div>
+        <p>Cette page n'existe pas...</p>
+      </div>
+    </>
+  );
+}
