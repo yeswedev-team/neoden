959ce11128259a6fcaf4540025b284b33d3e811a
David Gourdet <davidgourdet@mac.localdomain>
2021-07-20 14:55:21 +0200

    Add minimal preview (WIP) on Gatsby Cloud

---
+++ sanity/resolveProductionUrl.js
@@ -0,0 +1,36 @@
+import { format, parseISO } from 'date-fns';
+
+const remoteURL = 'https://preview-neoden.gtsb.io';
+const localURL = 'http://localhost:8000';
+const previewURL =
+  window.location.hostname === 'localhost' ? localURL : remoteURL;
+
+console.log(window.location.hostname);
+console.log(previewURL);
+
+const isDraft = (id) => id.includes('drafts');
+
+export default function resolveProductionUrl(document) {
+  // console.log(document);
+
+  if (document._type === 'post') {
+    const dateSegment = format(parseISO(document.publishedAt), 'yyyy/MM');
+    return `${previewURL}/actualites/${dateSegment}/${document.slug.current}`;
+  }
+  if (document._type === 'page') {
+    if (document.slug.current === 'home') {
+      return `${previewURL}/`;
+    }
+    return `${previewURL}/${document.slug.current}`;
+  }
+  // if (document._type === 'singletonFAQ') {
+  //   return `${previewURL}/faq`;
+  // }
+  // if (document._type === 'testimony') {
+  //   return `${previewURL}/temoignages/${document.slug.current}`;
+  // }
+  // if (document._type === 'realisation') {
+  //   return `${previewURL}/realisations/${document.slug.current}`;
+  // }
+  return undefined;
+}
