d2ddf30e847430c740bf3b5fc52443bcaccefcf6
alban.g <alban.g@yeswedev.fr>
2022-07-11 15:36:57 +0200

    specify dataset in client-config.js

--- web/client-config.js
+++ web/client-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   sanity: {
     projectId: process.env.GATSBY_SANITY_PROJECT_ID || 'qqvo71j5',
-    dataset: process.env.BRANCH,
+    dataset: process.env.BRANCH || 'production',
   },
 };
